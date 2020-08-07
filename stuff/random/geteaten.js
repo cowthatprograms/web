7420ebb91e83bc24eedc776fc5fe086101ead0eb
Jacob Chin <you@example.com>
2020-08-07 13:28:57 -0400

    fix

--- stuff/random/geteaten.js
+++ stuff/random/geteaten.js
@@ -1,3 +1,10 @@
-window.onload = function() {
-  document.getElementById('geteaten').style.fontSize = '15em';
+window.onload = function(x) {
+  if (x.matches) {
+    document.getElementById('geteaten').style.fontSize = '96px';
+  } else {
+    document.getElementById('geteaten').style.fontSize = '15em';
+  }
 }
+
+var x = window.matchMedia("(max-width: 1000px)")
+x.addListener(myFunction)
