289d820d2b988ea6598e74c8e9c07d3798fc721d
Jacob Chin <you@example.com>
2020-08-07 14:09:29 -0400

    fadein

---
+++ stuff/random/fadein.js
@@ -0,0 +1,18 @@
+var txt;
+
+async function fadein() {
+  document.getElementById('background').style.display = 'block';
+  await sleep(500);
+  document.getElementById('fadeintext').innerHTML = txt;
+  document.getElementById('fadeintext').style.fontSize = '15em';
+}
+
+function input() {
+  txt = document.getElementById('textinput').value;
+  document.getElementById('textinput').style.display = 'none';
+  fadein();
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
--- stuff/random/geteaten.js
+++ stuff/random/geteaten.js
@@ -1,10 +1,3 @@
-window.onload = function(x) {
-  if (x.matches) {
-    document.getElementById('geteaten').style.fontSize = '96px';
-  } else {
-    document.getElementById('geteaten').style.fontSize = '15em';
-  }
+window.onload = function() {
+  document.getElementById('geteaten').style.fontSize = '15em';
 }
-
-var x = window.matchMedia("(max-width: 1000px)")
-x.addListener(myFunction)
