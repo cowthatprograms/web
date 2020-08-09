5807e0372371b73b9eb25b11e82e3587ddd33c6e
Jacob Chin <you@example.com>
2020-08-09 09:55:34 -0400

    fadein

--- stuff/random/fadein/fadein.js
+++ stuff/random/fadein/fadein.js
@@ -2,8 +2,9 @@ var txt;
 
 async function fadein() {
   document.getElementById('background').style.display = 'block';
-  await sleep(500);
   document.getElementById('fadeintext').innerHTML = txt;
+  document.getElementById('fadeintext').style.display = 'block';
+  await sleep(500);
   document.getElementById('fadeintext').style.fontSize = '15em';
 }
 
