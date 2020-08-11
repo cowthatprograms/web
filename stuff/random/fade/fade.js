61097d46773941037befff7333f0826c470137b2
Jacob Chin <you@example.com>
2020-08-10 21:34:00 -0400

    a

--- stuff/random/fade/fade.js
+++ stuff/random/fade/fade.js
@@ -2,6 +2,7 @@ window.onload = function() {
   var url_string = window.location.href;
   var url = new URL(url_string);
   var txt = url.searchParams.get("txt");
+  if (txt[0] === '/') { txt = txt.substring(1); }
   document.getElementById('text').innerHTML = txt;
   document.getElementById('text').style.fontSize = '15em';
 }
