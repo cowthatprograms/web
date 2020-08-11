361de95adfbd4635c52231497b7904ddcdeb3949
Jacob Chin <you@example.com>
2020-08-11 12:23:22 -0400

    fade in finished

--- stuff/random/fade-in/fade-in.js
+++ stuff/random/fade-in/fade-in.js
@@ -1,12 +1,15 @@
 var txt;
 
 async function fadein() {
+  document.getElementById('title').style.display = 'none';
   document.getElementById('background').style.display = 'block';
   document.getElementById('fadeintext').innerHTML = txt;
   document.getElementById('fadeintext').style.display = 'block';
   await sleep(500);
   document.getElementById('fadeintext').style.opacity = '1';
   document.getElementById('fadeintext').style.fontSize = '15em';
+  await sleep(1000);
+  document.getElementById('btn').style.top = '32px';
 }
 
 function input() {
@@ -17,4 +20,17 @@ function input() {
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function copy() {
+  var copyText = `http://legendcow.com/stuff/random/fade?txt=${txt}`;
+  navigator.clipboard.writeText(copyText);
+  var copyAlert = document.getElementById('copyAlert');
+  copyAlert.style.display = 'block';
+  await sleep(100);
+  copyAlert.style.opacity = '1';
+}
+
+function hide() {
+  document.getElementById('copyAlert').style.opacity = '0';
 }
