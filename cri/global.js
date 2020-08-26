6c50df75c04eaa721439ca70431db517b274745d
Jacob Chin <you@example.com>
2020-08-26 18:28:34 -0400

    added stuff

---
+++ cri/global.js
@@ -0,0 +1,16 @@
+// reload page on resize
+$(window).bind('resize', function() {
+  this.location.reload(false);
+});
+
+function showBorder(id) {
+  let elem = document.getElementById(id);
+  elem.style.borderBottom = '2px solid #845EC2';
+  elem.style.transform = 'translateY(-2px)';
+}
+
+function hideBorder(id) {
+  let elem = document.getElementById(id);
+  elem.style.borderBottom = '2px solid rgba(0, 0, 0, 0)';
+  elem.style.transform = 'translateY(0px)';
+}
---
+++ cri/loader.js
@@ -0,0 +1,34 @@
+onload = function() {
+  refresh();
+}
+
+async function refresh() {
+  // await sleep(1000)
+  // remove loading screen
+  let wrapper = document.getElementById('loader-wrapper')
+  wrapper.style.opacity = '0';
+  let content = document.getElementById('content');
+  
+  // insert navbar
+  let navbar = document.getElementById('navbar');
+  navbar.style.top = '50%';
+
+  // insert title
+  let title = document.getElementById('title');
+  if (window.innerWidth > 1280) {
+    title.style.marginTop = '10vh';
+  } else {
+    title.style.marginTop = '0';
+  }
+
+  await sleep(500)
+  content.style.opacity = '1'; // fade in content
+  await sleep(500) // wait for fade in
+  // remove transition and wrapper
+  content.style.transition = 'none';
+  document.body.removeChild(wrapper)
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
