31dd2a910bdbdd16fb3eefcc25d48d0cd937700d
Jacob Chin <you@example.com>
2020-08-27 20:02:00 -0400

    cri

--- cri/downloads.js
+++ cri/downloads.js
@@ -1,6 +1,7 @@
 function download(num) {
   let filename;
   if (num === 1) {filename = 'cri-encode.py'}
+  if (num === 3) {filename = 'cri-decode.py'}
   fetch('http://legendcow.com/cri/tools/' + filename)
   .then(resp => resp.blob())
   .then(blob => {
@@ -8,7 +9,7 @@ function download(num) {
     const a = document.createElement('a');
     a.style.display = 'none';
     a.href = url;
-    a.download = 'lol.py';
+    a.download = filename;
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);
