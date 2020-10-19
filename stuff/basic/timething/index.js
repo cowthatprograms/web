f8032e4d7dea4f834f1beca2f6b89869ab2eace3
cowthatprograms <cowthatprograms@gmail.com>
2020-10-18 22:32:23 -0400

    e

--- stuff/basic/timething/index.js
+++ stuff/basic/timething/index.js
@@ -16,3 +16,27 @@ function timeToEmotes(time) {
   while (output.length < 31) { output += '▬'; }
   return output;
 }
+
+function copyOutput() {
+  copyTextToClipboard(output.value);
+}
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+}
