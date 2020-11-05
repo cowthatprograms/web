3ab94c2b241eb0cbd9ca76abedb01a1968e0a758
cowthatprograms <cowthatprograms@gmail.com>
2020-11-05 14:41:23 -0500

    e

--- stuff/charlie/quadratics/quadratics.js
+++ stuff/charlie/quadratics/quadratics.js
@@ -10,13 +10,8 @@ onload = function() {
 function solve() {
   let answer = quadratic(Number(a.value), Number(b.value), Number(c.value));
   if (!isNaN(answer[0])) {
-    try {
-      a1.innerHTML = answer[0].toFixed(decimals.value);
-      a2.innerHTML = answer[1].toFixed(decimals.value);
-    } catch {
-      a1.innerHTML = answer[0];
-      a2.innerHTML = answer[1];
-    }
+    a1.innerHTML = answer[0].toFixed(decimals.value);
+    a2.innerHTML = answer[1].toFixed(decimals.value);
   } else {
     a1.innerHTML = '';
     a2.innerHTML = '';
