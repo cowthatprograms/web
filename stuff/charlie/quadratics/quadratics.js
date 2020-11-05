5bc379f75b3ba791d63a924e95b27e16b825a7ef
cowthatprograms <cowthatprograms@gmail.com>
2020-11-05 14:24:33 -0500

    quadratics

---
+++ stuff/charlie/quadratics/quadratics.js
@@ -0,0 +1,23 @@
+onload = function() {
+  a = document.getElementById('a');
+  b = document.getElementById('b');
+  c = document.getElementById('c');
+  a1 = document.getElementById('answer1');
+  a2 = document.getElementById('answer2');
+}
+
+function solve(decimals) {
+  let answer = quadratic(Number(a.value), Number(b.value), Number(c.value));
+  if (!isNaN(answer[0])) {
+    a1.innerHTML = answer[0];
+    a2.innerHTML = answer[1];
+  } else {
+    a1.innerHTML = '';
+    a2.innerHTML = '';
+  }
+}
+
+function quadratic(a, b, c) {
+  let squareroot = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+  return [(-b + squareroot) / (2 * a), (-b - squareroot) / (2 * a)]
+}
